92d059e99b93e31d5bd586cf30fde4ed7e5bf8c9
Tharuabi <tharani.s2023it@sece.ac.in>
2026-04-09 09:41:21 +0530

    fixed  api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://backend-startup-4.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'https://backend-startup-uljt.onrender.com',
   withCredentials: true,
 });
 
